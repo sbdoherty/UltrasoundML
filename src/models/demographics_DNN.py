b08a00e62c01da520eea9741b22c51a2dcbff5c2
SBD <sbdoherty@yahoo.com>
2022-06-28 12:42:52 -0400

    cleaner plotting code

--- src/models/demographics_DNN.py
+++ src/models/demographics_DNN.py
@@ -12,6 +12,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
+from datetime import datetime
 
 
 def ohe_df(enc, df, columns):
@@ -35,9 +36,35 @@ def plot_loss(head_tail, history):
     plt.ylabel('Error')
     plt.legend()
     plt.grid(True)
+    date = datetime.now().strftime("%Y_%m_%d-%I%p")
+
     if not os.path.exists(os.path.join(head_tail[0], "..", "Pictures")):
         os.mkdir(os.path.join(head_tail[0], "..", "Pictures"))
-    plt.savefig(os.path.join(head_tail[0], "..", "Pictures", "loss_history.png"))
+    plt.savefig(os.path.join(head_tail[0], "..", "Pictures", f"demographics_loss_history_{date}.png"))
+
+
+def plot_test_predictions(head_tail, model, test_df, test_labels):
+    """Function to visualize the quality of predictions on the test data"""
+    test_predictions = model.predict(test_df).flatten()
+    plt.figure()
+    plt.scatter(test_labels, test_predictions)
+    plt.xlabel('True values (Stiffness)')
+    plt.ylabel('Predictions (Stiffness)')
+    lims = [0, 0.02]
+    plt.xlim(lims)
+    plt.ylim(lims)
+    plt.plot(lims, lims)
+    date = datetime.now().strftime("%Y_%m_%d-%I%p")
+
+    plt.savefig(os.path.join(head_tail[0], "..", "Pictures", f"stiffness_DNN_demographics_{date}.png"), format='png')
+
+    # Second plot of histogram mape
+    plt.figure()
+    error = abs(test_predictions - test_labels)/test_labels * 100
+    plt.hist(error, bins=25)
+    plt.xlabel('Absolute Percent Error')
+    plt.ylabel('Count')
+    plt.savefig(os.path.join(head_tail[0], "..", "Pictures", f"demographics_error_{date}.png"), format='png')
 
 
 def build_and_compile_model(norm):
@@ -61,9 +88,9 @@ def tf_demographics(csv, categorical_features, numerical_features="", interface=
     # Read in the master_csv file
     dataset = pd.read_csv(csv)
     categorical_features.sort()
-    numerical_features.sort() #  need sorted numerical features to reconstruct the df
-
+    numerical_features.sort()  # need sorted numerical features to reconstruct the df
     features = [*categorical_features, *numerical_features]
+
     # Filter the csv by the desired features
     dataset = dataset[features]
     dataset.apply(lambda x: pd.to_numeric(x, errors='coerce').notnull().all())
@@ -73,18 +100,19 @@ def tf_demographics(csv, categorical_features, numerical_features="", interface=
     print(dataset.dtypes)
 
     # Plotting of numeric variables, not necessary for DNN
-    try:
-        plt.figure(1)
+    if numerical_features:
+        plt.figure()
         sns.pairplot(dataset[numerical_features], diag_kind='kde')
         head_tail = os.path.split(csv)
-        print(f'Saving seaborn picture to {os.path.join(head_tail[0], "..", "Pictures", "sns_plotting.png")}')
+        date = datetime.now().strftime("%Y_%m_%d-%I%p")
+        print(f'Saving seaborn picture to {os.path.join(head_tail[0], "..", "Pictures", f"sns_plotting_{date}.png")}')
+
         # Check for valid dir
         if not os.path.exists(os.path.join(head_tail[0], "..", "Pictures")):
             os.mkdir(os.path.join(head_tail[0], "..", "Pictures"))
-        plt.savefig(os.path.join(head_tail[0], "..", "Pictures", "sns_plotting.png"), format='png')
+        plt.savefig(os.path.join(head_tail[0], "..", "Pictures", f"sns_plotting_{date}.png"), format='png')
         plt.close()
-    except:
-        raise Exception("Seaborn Plotting did not work (not essential for training)")
+
 
     # Remove null values from the dataset
     dataset.apply(lambda x: pd.to_numeric(x, errors='coerce').notnull().all())
@@ -103,28 +131,20 @@ def tf_demographics(csv, categorical_features, numerical_features="", interface=
     # Encode non-numeric data with one-hot encoding
     enc = ColumnTransformer([("OHE", OneHotEncoder(sparse=False, handle_unknown="ignore"), categorical_features)],
                             remainder="passthrough")
-    enc.fit(dataset)
-    ohe_dataset = ohe_df(enc, dataset, categorical_features)
+    enc.fit(dataset)  # fit the encoder to the datasets variables
 
-    # Convert back to pandas array
-    # one_hot_labels = enc.transform(dataset)
-    # print(one_hot_labels)
-    # new_columns = list()
-    # for col, values in zip(dataset[categorical_features].columns, enc.categories_):
-    #     new_columns.extend([col + '_' + str(value) for value in values])
-    # dataset = pd.DataFrame([dataset[numerical_features], pd.DataFrame(one_hot_labels, columns=new_columns)], axis='columns')
+    # call function to onehot encode the data
+    ohe_dataset = ohe_df(enc, dataset, categorical_features)
 
     # Split training and testing data
     train_data, test_data = train_test_split(ohe_dataset, train_size=0.8, random_state=len(dataset))
     print(f"train data shape is {train_data.shape}")
     print(f"test data shape is {test_data.shape}")
+    print(train_data.describe().transpose()[['mean', 'std']])
 
     # Pop off the Total stiffness metric, which is our label
     train_labels = train_data.pop("Total_Stiff")
     test_labels = test_data.pop("Total_Stiff")
-
-    print(train_data.shape)
-    print(train_data.describe().transpose()[['mean', 'std']])
 
     #  Normalize the inputs
     normalizer = tf.keras.layers.Normalization(axis=-1)
@@ -143,18 +163,21 @@ def tf_demographics(csv, categorical_features, numerical_features="", interface=
     es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=200)
     model = build_and_compile_model(normalizer)
     model.summary()
-    print(train_data)
-    print(train_labels)
     batch_size = 32
-    print(len(train_data))
+
+    # create a log directory for a tensorboard visualization
+    if not os.path.exists(os.path.join(head_tail[0], "..", "logs", "fit")):
+        os.mkdir(os.path.join(head_tail[0], "..", "logs", "fit"))
+
+
     history = model.fit(
         train_data,
         train_labels,
         validation_split=0.2,
-        verbose=2, epochs=750,
+        verbose=2, epochs=1000,
         shuffle=True, batch_size=batch_size,
     )
-
+    # visualize model loss
     plot_loss(head_tail, history)
 
     # print out metrics on how the model performed on the test data
@@ -164,26 +187,8 @@ def tf_demographics(csv, categorical_features, numerical_features="", interface=
     print(f"model mean squared error is: {score[2]}")
     print(f"model mean average percent error is {score[3]}")
 
-    # Plot predictions vs true values to visually assess accuracy
-    test_predictions = model.predict(test_data).flatten()
-    plt.figure(3)
-    plt.scatter(test_labels, test_predictions)
-    plt.xlabel('True values (Stiffness)')
-    plt.ylabel('Predictions (Stiffness)')
-    lims = [0, 0.02] #  Hard coded for now ???
-    plt.xlim(lims)
-    plt.ylim(lims)
-    plt.plot(lims, lims)
-    plt.savefig(os.path.join(head_tail[0], "..", "Pictures", "stiffness_DNN_demographics.png"), format='png')
-
-    # Plot of error in a histogram bin
-    plt.figure(4)
-    error = test_predictions - test_labels
-    plt.hist(error, bins=25)
-    plt.xlabel('Prediction Error [Stiffness]')
-    plt.ylabel('Count')
-    plt.savefig(os.path.join(head_tail[0], "..", "Pictures", "demographics_error.png"), format='png')
-
+    # Plot predictions vs true values to visually assess accuracy and histogram of APE distribution
+    plot_test_predictions(head_tail, model, test_data, test_labels)
 
     # Generate a gradio web interface if the user requested it
     if interface:
@@ -191,7 +196,7 @@ def tf_demographics(csv, categorical_features, numerical_features="", interface=
 
         def make_prediction(*features):
             """Function to make a prediction on a new user value from a gradio user interface"""
-            Total_Stiff = 999  # Dummy val just for the one hot encoder
+            Total_Stiff = 999  # Dummy val just for the one hot encoder. Probably a more graceful solution available?
             features = list(features)
             features.append(Total_Stiff)
             cols = categorical_features + numerical_features
@@ -200,9 +205,12 @@ def tf_demographics(csv, categorical_features, numerical_features="", interface=
             # Convert user input to one hot encoded values so predictions can be made
             ohe_pred = ohe_df(enc, pred_df, categorical_features)
             ohe_pred.drop(["Total_Stiff"], axis=1, inplace=True)
+
+            # Sanity check that variables look correct
             with pd.option_context('display.max_rows', None, 'display.max_columns', None):
                 print(ohe_pred)
             pred = model.predict([ohe_pred])
+
             print(f"Predicted tissue stiffness of {pred[0][0]} MPa/mm")
             return f"Predicted tissue stiffness of {pred[0][0]} MPa/mm"
 
@@ -228,13 +236,13 @@ def tf_demographics(csv, categorical_features, numerical_features="", interface=
 
         #  Launch gradio interface on the web
         app = gr.Interface(fn=make_prediction, inputs=input_list, outputs="text")
-        app.launch(share=True)
+        app.launch(share=False) # share=True to display on the gradio webpage. Can share on huggingfaces
 
 
 if __name__ == "__main__":
     csv_path = r"F:\WorkData\MULTIS\master_csv\001_MasterList_indentation.csv"
     #  lists are sorted alphabetically so order does not matter
-    categorical_features = ["SubID", "Location", "Gender", "ActivityLevel", "Race", "Ethnicity"] #  Features that aren't numerical
-    numeric_features = ["Total_Stiff", "Age", "BMI"] #  Features defined by a range of numbers
-    make_interface = True
+    categorical_features = ["SubID", "Location", "Gender", "ActivityLevel", "Race", "Ethnicity"]  # Features that aren't numerical
+    numeric_features = ["Total_Stiff", "Age", "BMI"]  # Features defined by a range of numbers
+    make_interface = False
     tf_demographics(csv_path, categorical_features=categorical_features, numerical_features=numeric_features, interface=make_interface)
